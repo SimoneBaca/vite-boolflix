1d8f89c357ebf8efc369e6410d6eac2c49e96de3
Simonebaca <simobaco99@gmail.com>
2023-04-19 17:20:41 +0200

    add structure and search bar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { store } from './components/store'
+import AppContent from './components/App.Content.vue';
+export default {
+  
+  components:{
+    AppContent,
+  },
+  data(){
+    return{
+      store,
+    }
+  }
+}
 </script>
+<template lang="">
+<div>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<AppContent></AppContent>
+</div>
 </template>
+<style lang="">
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/cardFilm.vue
@@ -0,0 +1,17 @@
+<script>
+
+import { store } from './store';
+export default {
+    props:{
+        movie: Object,
+    }
+}
+</script>
+<template lang="">
+<div>
+
+</div>
+</template>
+<style lang="">
+
+</style>
